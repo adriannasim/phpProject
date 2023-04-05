0024a3f79ad844d0f15c10c126c3955c92773bbb
tzwynn02 <129135416+tzwynn02@users.noreply.github.com>
2023-04-05 11:23:48 +0800

    Add files via upload
    merch.php, css and images

---
+++ merch.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Merch</title>
+        <link href="css/merch.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="logo">
+            <img src="img/sample1.png" alt="" width="100%"/>
+        </div>
+        <h1>Merch</h1>
+        <div class="merch">
+            <div class="content">
+                <img src="img/m1-front.jpg"  alt="" />
+                <h3>Gaming Controller T-Shirt</h3>
+                <h5>RM 40</h5>
+                <button class="buy1">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m2-front.jpg"  alt="" />
+                <h3>Typical Gamer Baseball Cap</h3>
+                <h5>RM 35</h5>
+                 <button class="buy2">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m3-front.jpg"  alt="" />
+                <h3>Saving the World by Levels Tote Bag</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m4-front.jpg"  alt="" />
+                <h3>I Paused My Game Tote Bag</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m5-back1.jpg"  alt="" />
+                <h3>Typical Gamer Hoodie</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m6-front.jpg"  alt="" />
+                <h3>Typical Gamer Sweater</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m7-front.jpg"  alt="" />
+                <h3>Hipster T-Shirt</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+            <div class="content">
+                <img src="img/m8-front.jpg"  alt="" />
+                <h3>Air Force Gaming Baseball Cap</h3>
+                <h5>RM 35</h5>
+                 <button class="buy3">Buy Now</button>
+            </div>
+        </div>
+    </body>
+</html>
