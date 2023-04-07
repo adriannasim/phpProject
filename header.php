b5aa97035d943b99d38f1f4bbe0380e4a061a2fe
adriannasim <adriannasim@gmail.com>
2023-04-07 22:55:19 +0800

    moved files around

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
+        <div class="header-logo"><img src="img/logo/logo.png"/></div>
+
     </body>
 </html>
