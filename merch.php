2ba4689d1ad9ed90135df7d9c50df3e47b718c5d
adriannasim <adriannasim@gmail.com>
2023-04-07 22:55:45 +0800

    moved files around

--- merch.php
+++ merch.php
@@ -6,58 +6,82 @@
     </head>
     <body>
         <div class="logo">
-            <img src="img/sample1.png" alt="" width="100%"/>
+            <img src="img/logo.png" alt="" width="100%"/>
+        </div>
+        <div class="mbanner">
+            <img src="img/merch-banner.png" />
         </div>
         <h1>Merch</h1>
+        <div class="merchfeature">
+            <table class="merchfunc">
+                <tr>
+                    <td><button class="cart">Cart</button></td>
+                    <td><button class="history">History</button></td>
+                    <td><button class="filter" onclick="showHideFilter()">Filter<p>&#9660;</p></button></td>
+                </tr>
+            </table>
+            <section class="hidden">
+                <table>
+                    <tr>
+                        <td><button class="all" >All</button></td>
+                        <td><button class="tshirt" >T-Shirt</button></td>
+                        <td><button class="hoodie" >Hoodie/Sweater</button></td>
+                        <td><button class="hats" >Hats</button></td>
+                        <td><button class="totebag" >Tote Bags</button></td>
+                    </tr>
+                </table>             
+            </section>
+        </div>
         <div class="merch">
-            <div class="content">
+            <div class="content" >
                 <img src="img/m1-front.jpg"  alt="" />
                 <h3>Gaming Controller T-Shirt</h3>
-                <h5>RM 40</h5>
+                <h5>RM 50</h5>
                 <button class="buy1">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m2-front.jpg"  alt="" />
                 <h3>Typical Gamer Baseball Cap</h3>
-                <h5>RM 35</h5>
-                 <button class="buy2">Buy Now</button>
+                <h5>RM 40</h5>
+                <button class="buy2">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m3-front.jpg"  alt="" />
                 <h3>Saving the World by Levels Tote Bag</h3>
                 <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <button class="buy3">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m4-front.jpg"  alt="" />
                 <h3>I Paused My Game Tote Bag</h3>
                 <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <button class="buy3">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m5-back1.jpg"  alt="" />
                 <h3>Typical Gamer Hoodie</h3>
-                <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <h5>RM 80</h5>
+                <button class="buy3">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m6-front.jpg"  alt="" />
                 <h3>Typical Gamer Sweater</h3>
-                <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <h5>RM 75</h5>
+                <button class="buy3">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m7-front.jpg"  alt="" />
                 <h3>Hipster T-Shirt</h3>
-                <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <h5>RM 50</h5>
+                <button class="buy3">Buy Now</button>
             </div>
-            <div class="content">
+            <div class="content" >
                 <img src="img/m8-front.jpg"  alt="" />
                 <h3>Air Force Gaming Baseball Cap</h3>
-                <h5>RM 35</h5>
-                 <button class="buy3">Buy Now</button>
+                <h5>RM 40</h5>
+                <button class="buy3">Buy Now</button>
             </div>
         </div>
+        <script src="merch.js"></script>
     </body>
 </html>
