61b4f1a68c3447430e274a2c8c8007794dc3d7d4
choongxinyuan <xinyuan270704@gmail.com>
2023-04-08 23:54:20 +0800

    hi

--- ticket.php
+++ ticket.php
@@ -11,12 +11,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
   </head>
   
   <body>
+    <?php include "headerUser.php"?>
       <h1>Ticket Site</h1>
       <h2>Upcoming Events</h2>
       
-        <div class="container">
-            <img src="b2.png" alt="MLBB" class="image">
-            <div class="overlay">
+        <div class="ticket-container">
+            <img src="b2.png" alt="MLBB" class="ticket-container-image">
+            <div class="ticket-overlay">
                 <h3>MLBB Tournament Grand Finale</h3>
                 <p><b>Date:</b> April 28, 2023</p>
                 <p><b>Venue:</b> CA, TAR UMT</b></p>
@@ -25,21 +26,23 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             </div> 
         </div>
       <br><br>
-        <div class="container">
-            <img src="b3.png" alt="Valorant" class="image">
-            <div class="overlay">
+        <div class="ticket-container">
+            <img src="b3.png" alt="Valorant" class="ticket-container-image">
+            <div class="ticket-overlay">
                 <h3>VALORANT: Battle Of The Ace</h3>
+                <div class="overlay2">
                 <p><b>Date:</b> May 20, 2023</p>
                 <p><b>Venue:</b> Foyer, TAR UMT</b></p>
                 <p><b>Participation Fee:</b> RM20 per pax</b></p>
                 <p><b>Spectator Fee:</b> RM40 per pax</b></p>
                 <button>Register Now</button>
+                </div>
             </div> 
         </div>
       <br><br>
-        <div class="container">
-            <img src="b4.png" alt="War Zone" class="image">
-            <div class="overlay">
+        <div class="ticket-container">
+            <img src="b4.png" alt="War Zone" class="ticket-container-image">
+            <div class="ticket-overlay">
                 <h3>War Zone S2</h3>
                 <p><b>Date:</b> June 12, 2023 (MON) - June 16, 2023 (FRI)</p>
                 <p><b>Venue:</b> CA, TAR UMT</b></p>
@@ -47,6 +50,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                 <button>Register Now</button>
             </div> 
         </div>
-            
+        <?php include "footerUser.php"?>        
   </body>
 </html>
