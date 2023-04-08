628054f7e8e55c3ad1693f1d71e92767cef20ddc
choongxinyuan <xinyuan270704@gmail.com>
2023-04-09 02:02:11 +0800

    .

--- ticket.php
+++ ticket.php
@@ -5,51 +5,54 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 -->
 <html>
 <head>
-    <title>Ticket Site</title>
+    <title>Buy Ticket</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="ticket.css">
-  </head>
+</head>
   
-  <body>
+<body>
     <?php include "headerUser.php"?>
-      <h1>Ticket Site</h1>
-      <h2>Upcoming Events</h2>
-      
-        <div class="ticket-container">
-            <img src="b2.png" alt="MLBB" class="ticket-container-image">
-            <div class="ticket-overlay">
-                <h3>MLBB Tournament Grand Finale</h3>
-                <p><b>Date:</b> April 28, 2023</p>
-                <p><b>Venue:</b> CA, TAR UMT</b></p>
-                <p><b>Ticket Price:</b> RM40 per pax</b></p>
-                <button>Buy Ticket</button>
+    <br>
+    <h1 style="text-align:center">Upcoming Events</h1>
+
+    <br><br>
+
+      <div class="ticket-container">
+          <img src="b2.png" alt="MLBB" class="ticket-container-image">
+          <div class="ticket-overlay">
+              <h2>MLBB Tournament Grand Finale</h2>
+              <div class="ticket-overlay1">
+                <button class="ticket-button">Buy Ticket</button>
+              </div>
+          </div> 
+      </div>
+
+    <br><br>
+
+      <div class="ticket-container">
+          <img src="b3.png" alt="Valorant" class="ticket-container-image">
+          <div class="ticket-overlay">
+              <h2>VALORANT: Battle Of The Ace</h2>
+              <div class="ticket-overlay1">
+                <button class="ticket-button">Register Now</button>
+              </div>
             </div> 
-        </div>
+      </div>
+
       <br><br>
-        <div class="ticket-container">
-            <img src="b3.png" alt="Valorant" class="ticket-container-image">
-            <div class="ticket-overlay">
-                <h3>VALORANT: Battle Of The Ace</h3>
-                <div class="overlay2">
-                <p><b>Date:</b> May 20, 2023</p>
-                <p><b>Venue:</b> Foyer, TAR UMT</b></p>
-                <p><b>Participation Fee:</b> RM20 per pax</b></p>
-                <p><b>Spectator Fee:</b> RM40 per pax</b></p>
-                <button>Register Now</button>
-                </div>
-            </div> 
-        </div>
-      <br><br>
-        <div class="ticket-container">
-            <img src="b4.png" alt="War Zone" class="ticket-container-image">
-            <div class="ticket-overlay">
-                <h3>War Zone S2</h3>
-                <p><b>Date:</b> June 12, 2023 (MON) - June 16, 2023 (FRI)</p>
-                <p><b>Venue:</b> CA, TAR UMT</b></p>
-                <p><b>Fee:</b> RM10 per pax</b></p>
-                <button>Register Now</button>
-            </div> 
-        </div>
-        <?php include "footerUser.php"?>        
-  </body>
+
+      <div class="ticket-container">
+          <img src="b4.png" alt="War Zone" class="ticket-container-image">
+          <div class="ticket-overlay">
+              <h2>War Zone S2</h2>
+              <div class="ticket-overlay1">
+                <button class="ticket-button">Register Now</button>
+              </div>
+          </div> 
+      </div>
+
+    <br><br>  
+
+    <?php include "footerUser.php"?>        
+</body>
 </html>
