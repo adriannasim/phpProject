a05e3d4d4d9b7924ab91130cad6b9b1ede887a35
adriannasim <adriannasim@gmail.com>
2023-04-09 01:53:08 +0800

    live agent chat

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>index</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <?php include "headerUser.php"; ?>
+        
+        <?php include "footerUser.php"; ?>
     </body>
 </html>
--- javascripts/helpdesk.js
+++ javascripts/helpdesk.js
@@ -1,9 +1,9 @@
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 
-function talkto() {
+(function() {
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
     s1.src='https://embed.tawk.to/6431968b31ebfa0fe7f735c3/1gtgr8554';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
-}
+})();
