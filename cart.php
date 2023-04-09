79942c527f0857d3662da405cffa5d468d2f2a9f
tzwynn02 <wynntze@gmail.com>
2023-04-09 14:06:05 +0800

    twww

--- cart.php
+++ cart.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div class="logo">
-            <img src="img/logo.png" alt="" width="100%"/>
+            <img src="img/merch/logo.png" alt="" width="100%"/>
         </div>
         <h1>Cart</h1>
         <div class="controlbtn">
@@ -18,7 +18,7 @@
             <div class="cart-section">
                 <div class="cart-details">
                     <div class="prod-box">
-                        <img src="img/m1-front.jpg" alt="alt"/>
+                        <img src="img/merch/m1-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Gaming Controller T-Shirt</h3>
                             <h4>Price: RM 50</h4>
@@ -28,7 +28,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m2-front.jpg" alt="alt"/>
+                        <img src="img/merch/m2-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Typical Gamer Baseball Cap</h3>
                             <h4>Price: RM 40</h4>
@@ -38,7 +38,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m3-front.jpg" alt="alt"/>
+                        <img src="img/merch/m3-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Saving the World by Levels Tote Bag</h3>
                             <h4>Price: RM 35</h4>
@@ -48,7 +48,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m4-front.jpg" alt="alt"/>
+                        <img src="img/merch/m4-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>I Paused My Game Tote Bag</h3>
                             <h4>Price: RM 35</h4>
@@ -58,7 +58,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m5-back.jpg" alt="alt"/>
+                        <img src="img/merch/m5-back.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Typical Gamer Hoodie</h3>
                             <h4>Price: RM 80</h4>
@@ -68,7 +68,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m6-front.jpg" alt="alt"/>
+                        <img src="img/merch/m6-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Typical Gamer Sweater</h3>
                             <h4>Price: RM 75</h4>
@@ -78,7 +78,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m7-front.jpg" alt="alt"/>
+                        <img src="img/merch/m7-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Hipster T-Shirt</h3>
                             <h4>Price: RM 50</h4>
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                     <div class="prod-box">
-                        <img src="img/m8-front.jpg" alt="alt"/>
+                        <img src="img/merch/m8-front.jpg" alt="alt"/>
                         <div class="box-content">
                             <h3>Air Force Gaming Baseball Cap</h3>
                             <h4>Price: RM 40</h4>
@@ -124,10 +124,10 @@
                         </div>
                         <h4>Select payment method</h4>
                         <div class="payment-img">
-                            <img src="img/mastercard.png">
-                            <img src="img/visa.png">
-                            <img src="img/paypal.png">
-                            <img src="img/amex.png">
+                            <img src="img/merch/mastercard.png">
+                            <img src="img/merch/visa.png">
+                            <img src="img/merch/paypal.png">
+                            <img src="img/merch/amex.png">
                         </div>
                         <div class="card-details">
                             <p>Card Owner</p>
