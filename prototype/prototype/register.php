88b30dde0feed68a43ac87e274ad0f2c6c89dc14
adriannasim <adriannasim@gmail.com>
2023-04-09 19:05:11 +0800

    uploded dhila's files

---
+++ prototype/prototype/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+    <meta charset="UFT-8">
+    <link href="../css/games.css" rel="stylesheet" type="text/css"/>
+    <body>
+    <header>
+        <h1>Registration</h1>
+    </header>
+    <main>
+    <form id="login_form" class="form_class" method="post">
+        <div class="form_div">
+            <label>Name:</label>
+            <input class="field_class" name="register_txt" type="text" placeholder="Enter Name" required>
+            <label>Email:</label>
+            <input class="field_class" name="register_txt" type="text" placeholder="Enter Email" required>
+            <label>Student ID:</label>
+            <input class="field_class" name="register_txt" type="text" placeholder="Enter StudentID" required>
+            <label>Create a password:</label>
+            <input id="pass" class="field_class" name="password_txt" type="password" placeholder="Create a password" required>
+            <label>Re-enter password:</label>
+            <input id="pass" class="field_class" name="password_txt" type="password" placeholder="Re-enter password" required>
+            <button class="submit_class" type="submit" name="login_btn">Enter</button>
+            
+    </form>
+</main>
+</<body>
+
+</<html>
+   
+    
