4d4589dddb8fcdc0b866279a8d6e83682c36e87f
adriannasim <adriannasim@gmail.com>
2023-04-09 23:53:15 +0800

    combined

---
+++ homepage.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homepage</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+        <header>
+            
+            <div class="flex">
+                <nav>
+                    <button id="nav-toggle" class="">
+                        <span class="strip"></span>
+                        <span class="strip"></span>
+                        <span class="strip"></span>
+                    </button>
+
+                        <ul id="nav-menu-container">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">games</a></li>
+                        <li><a href="#">games</a></li>
+                    </ul>
+                </nav>
+                <a href="login.php" id="login-register-button">Login/Register</a>
+            </div>
+        </header>
+
+        <main>
+            <section id="hero-image">
+                <div class="hero-marketing-text">
+                    <h1><b>Welcome to <span>Esports</span></b></h1>
+                    <h5><b>Greetings from our esports website! We are overjoyed to have you here and have you as a part of our expanding gaming and esports society.</b></h5>
+                </div>
+            </section>
+             <section id="latest-news">
+                <div class="flex">
+                    
+                    </div>
+                
+            </section>
+            <section id="game-types-boxes">
+                
+                <div class="flex">
+                    <div class="box new">
+                        <div class="shade"></div>
+                        <span class="badge new">New</span>
+                        <div class="contents">
+                            <h4><b>League of legends</b></h4>
+                            
+                        </div>
+                    </div>
+
+                    <div class="box strategy">
+                        <div class="shade"></div>
+                        <span class="badge strategy">New</span>
+                        <div class="contents">
+                            <h4><b>Fortnite</b></h4>
+                            
+                            
+                        </div>
+                    </div>
+
+                    <div class="box rpg">
+                        <div class="shade"></div>
+                        <span class="badge rpg">New</span>
+                        <div class="contents">
+                            <h4><b>Free fire</b></h4>
+                         
+                            
+                        </div>
+                    </div>
+                </div>
+           
+            </section>
+
+            <section id="tournaments">
+                <div class="flex">
+                    <span class="badge tournaments">Tournaments</span>
+                    <div class="box">
+                        <span class="badge premium-tournament">Premium Tournament</span>
+                        <div class="tournaments-image">
+                            
+                            
+                        </div>
+                        <div class="tournaments-content">
+                            <h3>MLBB Grand Finale</h3>
+                            <div><label>Tournament Begins:</label> <strong>28/4/2023</strong></div>
+                            <div><label>Time:</label> <strong>10AM - 4PM</strong></div>
+                            <div><label>Venue:</label> <strong>CA TAR UMT</strong></div>
+                            <div><label>Ticket Price:</label> <strong>RM:40</strong></div>
+                            <div><label>Tournament Organizer:</label> <strong>Admin</strong></div>
+                            
+                        </div>
+                    </div>
+
+                    <div class="box">
+                        <span class="badge premium-tournament">Premium Tournament</span>
+                        <div class="tournaments-image">
+                       
+                        </div>
+                        <div class="tournaments-content">
+                        <h3>Valorant</h3>
+                            <div><label>Tournament Begins:</label> <strong>20/5/2023</strong></div>
+                            <div><label>Time:</label> <strong>10AM - 4PM</strong></div>
+                            <div><label>Venue:</label> <strong>FOYER TAR UMT</strong></div>
+                            <div><label>Ticket Price:</label> <strong>RM:20</strong></div>
+                            <div><label>Tournament Organizer:</label> <strong>Admin</strong></div>    
+                        </div>
+                </div>
+                    <div class="box">
+                        <span class="badge premium-tournament">Premium Tournament</span>
+                        <div class="tournaments-image">
+                         
+                        </div>
+                        <div class="tournaments-content">
+                        <h3>War Zone S2</h3>
+                            <div><label>Tournament Begins:</label> <strong>12/6/2023</strong></div>
+                            <div><label>Time:</label> <strong>To be specified</strong></div>
+                            <div><label>Venue:</label> <strong>CA TAR UMT</strong></div>
+                            <div><label>Ticket Price:</label> <strong>RM:10</strong></div>
+                            <div><label>Tournament Organizer:</label> <strong>Admin</strong></div>    
+                        </div>
+            </section>
+        </main>
+
+        
+      
+    </body>
+</html>
