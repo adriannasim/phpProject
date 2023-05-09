25050eb8b680a1d431d03e00485b9f69f6af4124
chunchang14 <chunchang13@gmail.com>
2023-05-09 23:45:59 +0800

    Update new_seat.php

--- new_seat.php
+++ new_seat.php
@@ -1,38 +1,50 @@
 <!DOCTYPE html>
 <html>
-<?php 
-    session_start();
-    include ("config/config.php");
-    $UserID = "admin";
-    //$UserID = $_SESSION['UserID'];
+<?php
+session_start();
+include("config/config.php");
+$UserID = "admin";
+//$UserID = $_SESSION['UserID'];
 ?>
+
 <head>
     <meta charset="UTF-8">
     <style></style>
     <title>Add event seat</title>
 </head>
 <style>
-    form{
+    form {
         text-align: center;
         background-color: white;
     }
-    h1{
+
+    h1 {
         text-align: center;
 
     }
-    .event{
+
+    .event {
         text-align: center;
         font-size: 200%;
         color: black;
         padding: 25px;
     }
-    .seat{
+
+    .seattype {
+        text-align: center;
+        font-size: 200%;
+        color: black;
+        padding: 25px;
+    }
+
+    .seat {
         text-align: center;
         font-size: 200%;
         color: black;
         padding: 25px;
     }
 </style>
+
 <body>
     <?php
     include "headerAdmin.php"
@@ -42,29 +54,39 @@
     // Get data from database
     $sql = "SELECT * FROM event GROUP BY EventName";
     $result = $connection->query($sql);
+    $sql1 = "SELECT * FROM ticket_info GROUP BY TicketID";
+    $result1 = $connection->query($sql1);
+    $result2 = $connection->query($sql1);
     ?>
-        <form action="" method="POST">
-            <div class="form">
-        <div class="event">
-            <label for="event">Choose an event:</label>
-            <select name="event" id="event">
-                <?php while ($row = $result->fetch_assoc()) { ?>
-                    <option value="<?php echo $row['EventID']; ?>"><?php echo $row['EventName'];
-                } ?></option>
-            </select>
+    <form action="" method="POST">
+        <div class="form">
+            <div class="event">
+                <label for="event">Choose an event:</label>
+                <select name="event" id="event">
+                    <?php while ($row = $result->fetch_assoc()) { ?>
+                        <option value="<?php echo $row['EventID']; ?>"><?php echo $row['EventName'];
+                    } ?></option>
+                </select>
+            </div>
+            <div class="seattype">
+                <label for="type">Choose type of seat:<label>
+                        <select name="type" id="type">
+                            <?php while ($row = $result1->fetch_assoc()) { ?>
+                                <option value="<?php echo $row['TicketID']; ?>"><?php echo $row['TicketType'];
+                            } ?></option>
+                        </select>
             </div>
             <div class="seat">
-            <label for="amount">Choose amount of seat:<label>
-                    <select name="amount" id="amount">
-                    <?php while ($row = $result->fetch_assoc()) { ?>
-                    <option value="<?php echo $row['TicketID']; ?>"><?php echo $row['TicketQty'];
-                } ?></option>
-
-                        
+                <label for="amount">Choose amount of seat:<label>
+                        <select name="amount" id="amount">
+                            <?php while ($row = $result2->fetch_assoc()) { ?>
+                                <option value="<?php echo $row['TicketID']; ?>"><?php echo $row['TicketQty'];
+                            } ?></option>
+                        </select>
             </div>
             <input type="submit" value="Submit">
-            </div>
-        </form>
+        </div>
+    </form>
 
 </body>
 
