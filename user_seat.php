db9ed204d36795a636c6ab43774881ff75a9667f
choongxinyuan <xinyuan270704@gmail.com>
2023-05-20 00:13:32 +0800

    z

--- user_seat.php
+++ user_seat.php
@@ -468,7 +468,7 @@ if ($UserID == '') {
     }
     ?>
     <form action="" method="POST">
-    <a href="newTicket.php">Back</a>
+    <a href="ticketbuy.php?event_id=CA001">Back</a>
     </form>
     <?php include "footerUser.php"; ?>
 </body>
